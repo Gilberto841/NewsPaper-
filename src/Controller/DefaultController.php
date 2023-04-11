0b7fe87cd45b507b5b767a97878c87fa15528174
Gilberto841 <120499584+Gilberto841@users.noreply.github.com>
2023-04-11 20:36:46 +0200

    initial commit

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'show_home', methods: ['GET'])]
+    public function showHome(): Response
+    {
+        return $this->render('default/show_home.html.twig');
+    } // end function showHome()
+
+} // end class
